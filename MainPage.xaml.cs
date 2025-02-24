b1d625d9b65dfcec2448396c3f1224eb90bc0512
KarinaChiste <163331375+KarinaChiste@users.noreply.github.com>
2025-02-24 16:09:29 -0700

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CPRG211_Group1_Assignment2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CPRG211_Group1_Assignment2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
