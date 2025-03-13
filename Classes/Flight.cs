e34a7803f1c2d1cd0c55e55023725b58dc3a2e71
sinferna <jaspritgrewal@hotmail.com>
2025-03-12 18:19:05 -0600

    Got flight search logic functioning correctly (had to comment out Reservations to test code)

--- Classes/Flight.cs
+++ Classes/Flight.cs
@@ -8,21 +8,44 @@ namespace CPRG211_Group1_Assignment2.Classes
 {
     public class Flight
     {
+        public string ReservationCode { get; set; }
+        public string Airline { get; set; }
         public string OriginAirport { get; set; } = string.Empty;
         public string DestAirport { get; set; } = string.Empty;
         public string Day { get; set; } = string.Empty;
+        public string DepartureTime { get; set; }
+        public string Capacity { get; set; }
+        public string Price { get; set; }
 
-        public Flight()
+        //public Flight()
+        //{
+
+        //}
+
+        public Flight (string reservationCode, string airline, string originAirport, string destAirport, 
+            string day, string departureTime, string capacity, string price)
         {
-
-        }
-
-        public Flight (string originAirport, string destAirport, string day)
-        {
+            ReservationCode = reservationCode;
+            Airline = airline;
             OriginAirport = originAirport;
-            DestAirport = originAirport;
+            DestAirport = destAirport;
             Day = day;
+            DepartureTime = departureTime;
+            Capacity = capacity;
+            Price = price;
         }
-    }
 
+        public override string ToString()
+        {
+            return ReservationCode + ", " +
+                   Airline + ", " +
+                   OriginAirport + ", " +
+                   DestAirport + ", " +
+                   Day + ", " +
+                   DepartureTime + ", " +
+                   Capacity + ", " +
+                   Price;
+        }
+
+    }
 }
--- Classes/Reservation.cs
+++ Classes/Reservation.cs
@@ -1,44 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CPRG211_Group1_Assignment2.Exceptions;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using CPRG211_Group1_Assignment2.Exceptions;
 
-namespace CPRG211_Group1_Assignment2.Classes
-{
-    public class Reservation
-    {
+//namespace CPRG211_Group1_Assignment2.Classes
+//{
+//    public class Reservation
+//    {
         
-       public string ReservationCode { get; set; }
+//       public string ReservationCode { get; set; }
        
-       public string FullName { get; set; }
-        public string Citizenship { get; set; }
+//       public string FullName { get; set; }
+//        public string Citizenship { get; set; }
 
-        public Reservation(Flight flight, string name, string citizenship)
-        {
-           if (flight == null)
-            {
-                throw new EmptyFieldException();
-            }
-           else if (string.IsNullOrWhiteSpace(name))
-            {
-                throw new EmptyFieldException();
-            }
-            else if (string.IsNullOrWhiteSpace(citizenship))
-            {
-                throw new EmptyFieldException();
-            }
-            else
-            {
-                ReservationCode = "F"; // ADD NUMBER AFTER
-                FullName = name;
-                Citizenship = citizenship;
-            }
+//        public Reservation(Flight flight, string name, string citizenship)
+//        {
+//           if (flight == null)
+//            {
+//                throw new EmptyFieldException();
+//            }
+//           else if (string.IsNullOrWhiteSpace(name))
+//            {
+//                throw new EmptyFieldException();
+//            }
+//            else if (string.IsNullOrWhiteSpace(citizenship))
+//            {
+//                throw new EmptyFieldException();
+//            }
+//            else
+//            {
+//                ReservationCode = "F"; // ADD NUMBER AFTER
+//                FullName = name;
+//                Citizenship = citizenship;
+//            }
             
-        } 
-        public s
+//        } 
+//        public s
 
     
-    }
-}
+//    }
+//}
